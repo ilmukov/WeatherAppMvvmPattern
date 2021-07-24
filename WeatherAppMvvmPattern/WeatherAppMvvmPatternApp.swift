74e3ca30cf886b4d0cb083011c3103df0e7ea468
ilmukov <30494462+ilmukov@users.noreply.github.com>
2021-07-24 22:58:53 +0300

    Initial Commit

---
+++ WeatherAppMvvmPattern/WeatherAppMvvmPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppMvvmPatternApp.swift
+//  WeatherAppMvvmPattern
+//
+//  Created by Stepan Ilmukov on 24.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppMvvmPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
